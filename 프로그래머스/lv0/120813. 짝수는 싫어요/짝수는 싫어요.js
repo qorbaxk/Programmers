1c3992a5dcac5234ede1e454a136c75f388cfba7
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-20 15:15:33 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,7 @@
 const solution = (n) => {
     let arr = [];
-    for(i=1; i<=n; i++){
-        i%2!==0 ? arr.push(i) : null;
+    for(i=1; i<=n; i+=2){
+        arr.push(i);
     }
     return arr;
 }
