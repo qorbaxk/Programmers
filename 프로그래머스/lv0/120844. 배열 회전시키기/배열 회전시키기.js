e6c8fe0b4e708e24ca3674ba5e9a05bd80a4af18
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-21 17:13:47 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,12 @@
+const solution = (numbers, direction) => {
+    if(direction === 'right'){
+        numbers.unshift(numbers[numbers.length-1]);
+        numbers.pop();
+        return numbers;
+        
+    }else{
+        numbers.push(numbers[0]);
+        numbers.shift();
+        return numbers;  
+    }
+}
