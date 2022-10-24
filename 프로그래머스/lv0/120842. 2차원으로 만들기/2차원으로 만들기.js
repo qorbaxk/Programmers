9d3a333d26a1b918e29bbac5b2ea8bc26dcfcc2e
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-24 14:38:42 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,12 +1,10 @@
 const solution = (num_list, n) =>{
        
-        let cnt = Math.floor(num_list.length / n) + 
-            (Math.floor(num_list.length % n) > 0 ? 1 : 0);
-        let tmp = [];
- 
-        for (let i = 0; i < cnt; i++) {
-            tmp.push(num_list.splice(0, n));
-        }
- 
-        return tmp;
+    let tmp = [];
+    
+    while(num_list.length){
+        tmp.push(num_list.splice(0, n));
+    }
+      
+    return tmp;
 }
