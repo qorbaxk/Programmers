d06d79dae56e84baa76dc26b12a1ea63b1b9bbf5
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-25 22:15:14 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,23 @@
+const solution = (n) => {
+    let result = [];
+    
+    while(n%2 == 0){
+        result.push(2);
+        n /= 2;
+    }
+    
+    for(let i=3; i*i<=n; i+=2){
+        while(n%i == 0){
+            result.push(i);
+            n /= i;
+        }
+    }
+    
+    if(n>2){
+        result.push(n)
+    }
+    
+    const answer = new Set(result);
+    
+    return [...answer];
+}
