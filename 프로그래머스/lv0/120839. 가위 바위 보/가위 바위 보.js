9bbccecc71f5ee860a7841aa89872fcf5be5314a
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-27 00:47:33 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,3 +1,3 @@
 const solution = (rsp) => {
-    return rsp.split('').map(x=>x==='2'?x.replace('2','0'):x==='0'?x.replace('0','5'):x==='5'?x.replace('5','2'):x).join('')
+    return rsp.split('').map(x=>x==='2'?'0':x==='0'?'5':x==='5'?'2':x).join('')
 }
