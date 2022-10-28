192529e727d99959053ca40890feaaba02b6af4a
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-10-28 17:35:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,7 +1,9 @@
 const solution = (denum1, num1, denum2, num2) => {
     
     let getGCD = (a, b) => (b > 0 ? getGCD(b, a % b) : a);
-    let lcm = (num1*num2) / getGCD(num1,num2);
-    let nums = denum1*(parseInt(lcm/num1)) + denum2*(parseInt(lcm/num2));
-    return [nums/getGCD(nums,lcm),lcm/getGCD(nums,lcm)]
+    let denum = denum1*num2+denum2*num1;
+    let num = num1*num2;
+    let gcd = getGCD(denum,num)
+    
+    return [denum/gcd, num/gcd]
 }
