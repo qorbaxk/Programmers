f4319b4b29c6c8553f2c2d8d3c340bba5fe206d0
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-03 21:44:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,6 +1,5 @@
 const solution = (numbers) => {
     let arr = [0,1,2,3,4,5,6,7,8,9];
-    numbers = numbers.sort((a,b)=>a-b);
     let dif = arr.filter(x=>!numbers.includes(x));
     return dif.reduce((a,b)=>a+b,0)
 }
