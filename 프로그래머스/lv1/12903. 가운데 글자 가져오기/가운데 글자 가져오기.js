381ceb48ea70c537360f6c2078b1a7703262d49d
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-03 21:56:16 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,3 +1,3 @@
 const solution = (s) => {
-    return s.length%2 ? s[(s.length-1)/2] : s.slice((s.length/2)-1,(s.length/2)+1)
+    return s.length%2 ? s[(s.length-1)/2] : s.substr((s.length/2)-1,2)
 }
