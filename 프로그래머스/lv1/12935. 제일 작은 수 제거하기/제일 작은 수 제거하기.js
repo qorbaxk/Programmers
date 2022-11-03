e222fb348fbb5c231504f870e32d0cc93221a7c9
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-03 21:32:30 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,5 @@
 const solution = (arr) => {
-    if(arr.length===1) return [-1];
     let min = Math.min(...arr);
     arr = arr.filter(x=>x!==min);
-    return arr;
+    return arr.length?arr:[-1];
 }
