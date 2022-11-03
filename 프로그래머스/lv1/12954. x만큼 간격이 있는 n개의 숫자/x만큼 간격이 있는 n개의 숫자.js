42fcd659e5e0531239dbacd2501957f404cb6771
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-03 17:40:17 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.25 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,19 @@
+const solution = (x,n) => {
+    let answer = [];
+    
+    if(x>0){
+        for(let i=x; i<=x*n; i+=x){
+            answer.push(i)
+        }
+    }else if(x<0){
+         for(let i=x; i>=x*n; i+=x){
+            answer.push(i)
+        }
+    }else if(x===0){
+        for(let i=0; i<n; i++){
+            answer.push(0)
+        }
+    }
+    
+    return answer;
+}
