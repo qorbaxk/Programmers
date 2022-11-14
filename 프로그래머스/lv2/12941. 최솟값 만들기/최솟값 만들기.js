930b85b489d225c83ae030e90c1323f73739ce39
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-14 17:59:35 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,9 @@
+const solution = (A,B) => {
+    A = A.sort((a,b)=>a-b);
+    B = B.sort((a,b)=>b-a);
+    let sum=0;
+    for(let i=0; i<A.length; i++){
+        sum += A[i]*B[i];
+    }
+    return sum;
+}
