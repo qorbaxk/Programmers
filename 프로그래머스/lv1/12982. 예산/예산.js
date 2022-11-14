77184a7946ec35d55ca2e4be199c9f2ed3ac821e
Butter on Bread <97217443+qorbaxk@users.noreply.github.com>
2022-11-14 15:26:25 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+const solution = (d, budget) => {
+    d = d.sort((a,b)=>a-b);
+    let sum = 0;
+    let count = 0;
+    for(let i=0; i<d.length; i++){
+        if(sum+d[i]<=budget){
+            sum += d[i];
+            count++;
+        }  
+    }  
+    return count;  
+}
