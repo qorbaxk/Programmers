8f5f9bdfc030b2d1edb4044284bde77a5dc77569
Baxk <qorbaxk97@gmail.com>
2023-04-27 16:20:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,7 @@
-const solution = (n) => {
-    let arr = [];
-    for(i=1; i<=n; i+=2){
-        arr.push(i);
+function solution(n) {
+    const sum=[];
+    for(let i=1; i<=n; i+=2){
+        sum.push(i)
     }
-    return arr;
+    return sum
 }
