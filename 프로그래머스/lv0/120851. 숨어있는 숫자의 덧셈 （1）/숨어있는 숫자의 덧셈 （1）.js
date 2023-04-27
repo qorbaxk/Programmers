6ac1693d702082188dbcd714fa7eb816d758dee2
Baxk <qorbaxk97@gmail.com>
2023-04-27 16:36:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,6 +1,5 @@
-const solution = (my_string) => {
-    let regex = new RegExp('[^0-9]', 'g');
-    let result = my_string.replace(regex, '');
-    let arr = result.split('');
-    return arr.reduce((a,b)=>parseInt(a)+parseInt(b),0);
+function solution(my_string) {
+    regex = new RegExp('[^0-9]', 'g')
+    let newArr = my_string.replace(regex,'').split('')
+    return newArr.reduce((a,b)=>a + +b,0)
 }
