1a37bfc1ca721c4969606ebfad7955cf0bc9cb00
Baxk <qorbaxk97@gmail.com>
2023-04-27 17:14:23 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,1 +1,5 @@
-const solution = (hp) => parseInt(hp/5)+parseInt((hp%5)/3)+parseInt((hp%5)%3);
+function solution(hp) {
+    let result = 0;
+    result = parseInt(hp/5) + parseInt((hp%5)/3) + parseInt((hp%5)%3)
+    return result
+}
