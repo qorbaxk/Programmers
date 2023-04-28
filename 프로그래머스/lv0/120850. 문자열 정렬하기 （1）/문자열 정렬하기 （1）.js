df3991bbda743f30aa6f568437067cc7e0799aec
Baxk <qorbaxk97@gmail.com>
2023-04-28 16:11:21 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,2 +1,5 @@
-const solution = (my_string) => my_string.match(/[0-9]/g).sort((a,b)=>a-b).map(Number);
+function solution(my_string) {
+    my_string = my_string.replace(/[^0-9]/g,'')
+    return [...my_string].sort((a,b)=>a-b).map(v=>Number(v))
     
+}
