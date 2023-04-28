9aa80e79f3987de4629cbd4b6d8fccdb8de2e05a
Baxk <qorbaxk97@gmail.com>
2023-04-28 15:53:44 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,3 +1,3 @@
-const solution = (rsp) => {
-    return rsp.split('').map(x=>x==='2'?'0':x==='0'?'5':x==='5'?'2':x).join('')
+function solution(rsp) {
+   return [...rsp].map(v=>v==='2'?'0':v==='0'?'5':'2').join('')
 }
