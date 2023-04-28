9a8f4c357d7ece93f448016889f7960e54868139
Baxk <qorbaxk97@gmail.com>
2023-04-28 16:02:54 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,8 +1,5 @@
-const solution = (numbers) => {
-    numbers = numbers.sort((a,b)=>a-b);
-    let max1 = numbers[0]*numbers[1];
-    let max2 = numbers[(numbers.length)-1]*numbers[(numbers.length)-2];
-    let arr = [max1,max2];
-    return Math.max(...arr);
-    
+function solution(numbers) {
+    numbers.sort((a,b)=>a-b)
+    let max = [numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]]
+    return Math.max(...max)
 }
