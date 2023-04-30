3c5c20f40b0344d10963592e13484d0e8c39bbd2
Baxk <qorbaxk97@gmail.com>
2023-04-30 16:21:43 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,9 +1,11 @@
-const solution = (n) => {
-    let arr = [];
-    for(i=4; i<=n; i++){
+function solution(n) {
+    let count = 0;
+    for(let i=4; i<=n; i++){
         if(i!==5 && i!==7){
-           i%2===0 || i%3===0  || i%7===0 || i%5===0 ? arr.push(i) : null;
+            i!==5 && i!==7 && i%2===0 || i%3===0 || i%5===0 || i%7===0 ? count++ : null
         }
+            
     }
-    return arr.length;
+    
+    return count
 }
