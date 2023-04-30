718f6ad0eb307e7ffcec9efd9d69c81d3001f33b
Baxk <qorbaxk97@gmail.com>
2023-04-30 18:17:27 +0900

    [level 0] Title: k의 개수, Time: 11.00 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,7 +1,7 @@
-const solution = (i,j,k) => {
-    let result = 0;
-    for(let n=i; n<=j; n++){
-        result += n.toString().split('').filter(x=>x==k).length;
-    }
-    return result;
+function solution(i, j, k) {
+   let arr =[]
+    for(let t=i; t<=j; t++){
+       arr.push(t.toString())
+   }
+    return arr.join('').split('').filter(v=>v===k.toString()).length
 }
