3bcc8dc7689d01fd0a9b2baed898b77e41e66838
Baxk <qorbaxk97@gmail.com>
2023-04-30 14:04:39 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,10 +1,7 @@
-const solution = (n) => {
-
-    let getGCD = (num1, num2) => (num2 > 0 ? getGCD(num2, num1 % num2) : num1);
-    
-    if(6%n===0){
-        return 1;
-    }else{
-        return ((n*6)/getGCD(6,n))/6;
+function solution(n) {
+    for(let i=6; i<=600; i+=6){
+        if(i%n===0){
+            return i/6
+        }
     }
 }
