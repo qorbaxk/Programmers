fede336bc4eb53cf58b4722a177938471b7bfb46
Baxk <qorbaxk97@gmail.com>
2023-04-30 14:09:34 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let sum1 = 0;
+    let sum2 = 0;
+    for(let i=0; i<num_list.length; i++){
+       if(i%2){
+           sum1+=num_list[i]
+       } else{
+           sum2+=num_list[i]
+       }
+    }
+    return Math.max(sum1,sum2)
+}
