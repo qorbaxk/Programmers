f186e71958b691e0fba9ba4cfbdf00d775333d4b
Baxk <qorbaxk97@gmail.com>
2023-04-30 16:13:45 +0900

    [level 0] Title: 숫자 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,1 +1,5 @@
-const solution = (num,k) => num.toString().includes(k) ? num.toString().indexOf(k)+1 : -1
+function solution(num, k) {
+    k = String(k)
+    let idx = num.toString().split('').findIndex(v=>v===k)
+    return idx<0?-1:idx+1
+}
