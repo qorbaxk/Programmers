301dc0dbb49e4c28a495a95867a011cde6b9301d
Baxk <qorbaxk97@gmail.com>
2023-04-30 13:26:15 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,6 @@
-const solution = (my_string, num1, num2) => {
-    let str1 = my_string[num1];
-    let str2 = my_string[num2];
-    
-    return [...my_string].map((v,i)=>i===num1?v.replace(str1,str2):i===num2?v.replace(str2,str1):v).join('')
+function solution(my_string, num1, num2) {
+   let str = [...my_string]
+    str.splice(num1,1,my_string[num2])
+    str.splice(num2,1,my_string[num1])
+   return str.join('')
 }
