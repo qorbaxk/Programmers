0d3551fcd22a70ba86a187b7e5d8d04fa58a31cf
백승연 <qorbaxk97@gmail.com>
2024-03-23 08:44:12 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -13,11 +13,15 @@ function solution(clothes) {
     })
     
     let result = 1;
+    
     for(let [key, value] of map){
+        // 옷 종류 별로 곱해서 계산
+        // 이때 +1 하는 경우는 아무것도 안입을때를 추가적으로 계산
        result *= value+1
       
     }
     
+    // 모든 종류의 옷을 안 입는 1경우를 뺌
     return result-1
     
    
