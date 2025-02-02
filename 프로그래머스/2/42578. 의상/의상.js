5f549aff3c6a20777c0f788b3244b58c7cef079b
백승연 <qorbaxk97@gmail.com>
2025-02-02 16:49:22 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,29 +1,17 @@
 function solution(clothes) {
-    let map = new Map()
-    clothes.forEach(([value, key])=>{
-        
-        if(map.has(key)){
-            const prev = map.get(key)
-            map.set(key,prev+1)
+    const map = new Map()
+    // 맵객체를 사용하여 옷을 카테고리별로 분류함
+    for(let clo of clothes){
+        if(map.has(clo[1])){
+            map.set(clo[1], [...map.get(clo[1]),clo[0]])
         }else{
-        
-        map.set(key,1) 
+            map.set(clo[1], [clo[0]])       
         }
-        
-    })
-    
-    let result = 1;
-    
-    for(let [key, value] of map){
-        // 옷 종류 별로 곱해서 계산
-        // 이때 +1 하는 경우는 아무것도 안입을때를 추가적으로 계산
-       result *= value+1
-      
     }
-    
-    // 모든 종류의 옷을 안 입는 1경우를 뺌
-    return result-1
-    
-   
-    
+    /**
+    경우의 수 구하기
+    각 카테고리 별로 '선택안함' 의 조건이 포함되어있기 때문에
+    아이템 + 1(선택안함) 로 계산하며 각 카테고리에서 '모두 선택을 안하는 경우' 1가지를 제외해야 됨
+    */
+    return [...map.values()].reduce((a,b)=>a*(b.length+1),1) - 1
 }
