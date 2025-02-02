44bd234eb98cbbcbd0cb232e76fd00f892fb9c29
백승연 <qorbaxk97@gmail.com>
2025-02-02 14:42:35 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,12 @@
 function solution(nums) {
-    let count = nums.length/2
-    let no = new Set([...nums])
-    let min = Math.min(count, [...no].length)
-    return min
-    
+    // 폰켓몬의 갯수: 항상 짝수
+    const N = nums.length
+    // 가져갈 수 있는 최대 폰켓몬의 개수
+    const pickNum = N/2
+    // 중복을 제거한 폰켓몬의 배열
+    const noDup = new Set([...nums])
+    // 중복을 제거한 상태에서 고를 수 있는 최대 개수
+    const noDupSize = noDup.size
+    // 최솟값으로 맞추기
+    return Math.min(pickNum, noDupSize)
 }
